34c936ac8a782b929080ac7191246ed3c7874e76
Franco Vega <francovega04@gmail.com>
2025-06-27 02:00:08 -0300

    modificacion carrito

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,8 @@ export function Header() {
   const location = useLocation();
   const navigate = useNavigate();
   const {itemCart} = useCart();
+  let counter = itemCart.reduce((sum, product) => sum + product.quantity, 0);
+
   
   return (
     <section className="hd-header">
@@ -42,7 +44,7 @@ export function Header() {
             <path d="M0 0h24v24H0z" fill="none" />
             <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
           </svg>
-          <span className="counter">0</span>
+          <span className={counter > 0 ? "counter" : "counter hidden"}>{counter}</span>
         </Link>
       </div>
     </section>
